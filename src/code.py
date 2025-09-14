89406f3139c22d8dff8847abf1c3e110e19eff2c
Kiernan Nicholls <kiernann@protonmail.com>
2025-09-14 17:09:18 -0400

    Remove redundant autoreload

--- src/code.py
+++ src/code.py
@@ -26,11 +26,6 @@ def _wifi_connect():
     wifi.radio.connect(ssid, password)
     print("Connected, IP address:", wifi.radio.ipv4_address)
 
-try:
-    supervisor.disable_autoreload()
-except AttributeError:
-    supervisor.runtime.autoreload = False
-
 def main():
     mode = cfg.get("mode", "FANTASY_LOCAL")
     path = cfg.get("local_json_path", "/scoreboard.json")
